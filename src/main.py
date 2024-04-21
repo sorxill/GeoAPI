d0b26dd5fffbed36c7551e111456c44d690d4820
Yaroslav <yarikbritov@yandex.ru>
2024-04-21 12:09:58 +0300

    feat: create ping get rout for fastapi app

---
+++ src/main.py
@@ -0,0 +1,26 @@
+"""
+Main endpoint
+"""
+
+import uvicorn
+from fastapi import FastAPI
+
+
+geoapp = FastAPI(
+    title="GeoAPI",
+    description="Test API for test task",
+)
+
+
+@geoapp.get("/", description="Ping APP", name="Get rout For Ping")
+def start_answer():
+    """
+    Check the tests and e.t.c
+    """
+    return {
+        "answer": "Success",
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:geoapp", host="0.0.0.0", reload=True)
